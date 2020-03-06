ba2ad58ebcb994d430cdce5d4ad7a773a2b7860b
jack gaarkeuken <jack.gaarkeuken@gmail.com>
2020-03-06 17:56:59 -0500

    adding list

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import UserTable from "./tables/UserTable";
 const App = () => {
   // Data
   const usersData = [
-    { id: 1, name: "Tania", username: "floppydiskette" },
-    { id: 2, name: "Craig", username: "siliconeidolon" },
-    { id: 3, name: "Ben", username: "benisphere" }
+    { id: 1, name: "Jack", username: "Jackattack" },
+    { id: 2, name: "Chris", username: "Chris123" },
+    { id: 3, name: "Jon", username: "Jondoe" }
   ];
 
   const initialFormState = { id: null, name: "", username: "" };
